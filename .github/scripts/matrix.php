e4f1ab0ffc6a76d23b43a618c1daa5ab4fd3f7c2
Varun Sridharan <varunsridharan23@gmail.com>
2020-11-28 10:24:51 +0530

    Updated

---
+++ .github/scripts/matrix.php
@@ -0,0 +1,18 @@
+<?php
+#define( 'BASE_PATH', $_ENV['GITHUB_WORKSPACE'] );
+define( 'BASE_PATH', __DIR__ . './../../' );
+
+$paths  = glob( BASE_PATH . '*/*' );
+$return = array();
+foreach ( $paths as $path ) {
+	$image_name    = basename( dirname( $path ) );
+	$image_version = basename( $path );
+	$return[]      = array(
+		'name' => $image_name . ':' . $image_version,
+		'file' => $image_name . '/' . $image_version,
+	);
+}
+
+$return = json_encode( $return );
+
+echo "::set-output name=dockerinfo::$return";
